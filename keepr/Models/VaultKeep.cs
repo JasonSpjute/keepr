9782b35e785e8ef9fd5245da30686aa8b7c94bee
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2021-02-26 15:57:11 -0700

    created models and tables

--- keepr/Models/VaultKeep.cs
+++ keepr/Models/VaultKeep.cs
@@ -2,6 +2,9 @@ namespace keepr.Models
 {
     public class VaultKeep
     {
-        
+        public int Id { get; set; }
+        public string CreatorId { get; set; }
+        public int VaultId { get; set; }
+        public int KeepId { get; set; }
     }
 }
--- keepr/Setup.sql
+++ keepr/Setup.sql
@@ -1,10 +1,66 @@
 use keepr71;
 
-CREATE TABLE profiles
-(
-    id VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    name VARCHAR(255),
-    picture VARCHAR(255),
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE profiles
+-- (
+--     id VARCHAR(255) NOT NULL,
+--     email VARCHAR(255) NOT NULL,
+--     name VARCHAR(255),
+--     picture VARCHAR(255),
+--     PRIMARY KEY (id)
+-- );
+
+-- CREATE TABLE keeps
+-- (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     creatorId VARCHAR(255) NOT NULL,
+--     name VARCHAR(255) NOT NULL,
+--     description VARCHAR(255),
+--     img VARCHAR(255),
+--     views INT NOT NULL DEFAULT 0,
+--     shares INT NOT NULL DEFAULT 0,
+--     keeps INT NOT NULL DEFAULT 0,
+
+--     PRIMARY KEY (id),
+
+--     FOREIGN KEY (creatorId)
+--     REFERENCES profiles (id)
+--     ON DELETE CASCADE
+-- );
+
+
+-- CREATE TABLE vaults
+-- (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     creatorId VARCHAR(255) NOT NULL,
+--     name VARCHAR(255) NOT NULL,
+--     description VARCHAR(255),
+--     isPrivate TINYINT NOT NULL DEFAULT 0,
+
+--     PRIMARY KEY (id),
+
+--     FOREIGN KEY (creatorId)
+--     REFERENCES profiles (id)
+--     ON DELETE CASCADE
+-- );
+
+-- CREATE TABLE vaultKeeps
+-- (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     creatorId VARCHAR(255) NOT NULL,
+--     vaultId INT NOT NULL,
+--     keepId INT NOT NULL,
+
+--     PRIMARY KEY (id),
+
+--     FOREIGN KEY (vaultId)
+--     REFERENCES vaults (id)
+--     ON DELETE CASCADE,
+
+--     FOREIGN KEY (keepId)
+--     REFERENCES keeps (id)
+--     ON DELETE CASCADE,
+
+--     FOREIGN KEY (creatorId)
+--     REFERENCES profiles (id)
+--     ON DELETE CASCADE
+-- );
